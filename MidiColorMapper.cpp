3cdc50de36f0882d284d4783b103edca74ffc5a9
Hugo Hromic <hhromic@gmail.com>
2014-05-18 17:17:30 +0100

    Reworked MidiColorMapper class.

--- MidiColorMapper.cpp
+++ MidiColorMapper.cpp
@@ -1,57 +1,98 @@
 #include <cmath>
 #include <MidiColorMapper.h>
 
-// Class constructor
+// Class constructor/initialisation
 MidiColorMapper::MidiColorMapper() {
-    this->noteMin = 0x00;
-    this->noteMax = 0x7F;
-    this->mapper = COLOR_MAP;
-    this->noteColorMap = MidiNoteColors::NEWTON_1704;
-    this->fixedHue = 0x00;
+    for (uint8_t i=16; i--;)
+        reset(i);
+};
+
+// Get the minimum note value for a MIDI channel
+uint8_t MidiColorMapper::getNoteMin(uint8_t channel) {
+    return parameters[channel & 0xF].noteMin;
 }
 
-// Set minimum note
-void MidiColorMapper::setNoteMin(uint8_t noteMin) {
-    this->noteMin = noteMin;
+// Set the minimum note value for a MIDI channel
+void MidiColorMapper::setNoteMin(uint8_t channel, uint8_t noteMin) {
+    parameters[channel & 0xF].noteMin = noteMin & 0x7F;
 }
 
-// Set maximum note
-void MidiColorMapper::setNoteMax(uint8_t notemax) {
-    this->noteMax = noteMax;
+// Get the maximum note value for a MIDI channel
+uint8_t MidiColorMapper::getNoteMax(uint8_t channel) {
+    return parameters[channel & 0xF].noteMax;
 }
 
-// Set the active color mapper to use
-void MidiColorMapper::setMapper(Mappers mapper) {
-    this->mapper = mapper;
+// Set the maximum note value for a MIDI channel
+void MidiColorMapper::setNoteMax(uint8_t channel, uint8_t noteMax) {
+    parameters[channel & 0xF].noteMax = noteMax & 0x7F;
 }
 
-// Set the active note color map to use
-void MidiColorMapper::setNoteColorMap(MidiNoteColors::Maps noteColorMap) {
-    this->noteColorMap = noteColorMap;
+// Get the active color mapper for a MIDI channel
+MidiColorMapper::Mappers MidiColorMapper::getMapper(uint8_t channel) {
+    return parameters[channel & 0xF].mapper;
 }
 
-// Set the fixed color hue to use
-void MidiColorMapper::setFixedHue(uint8_t hue) {
-    this->fixedHue = hue;
+// Set the active color mapper to use for a MIDI channel
+void MidiColorMapper::setMapper(uint8_t channel, Mappers mapper) {
+    parameters[channel & 0xF].mapper = mapper;
 }
 
-// Map a MIDI note to an HSV color
-struct CHSV MidiColorMapper::map(uint8_t note, uint8_t velocity) {
-    struct CHSV noteColor;
-    switch (mapper) {
-        case COLOR_MAP: // Use Midi Note Color maps
-            noteColor = MidiNoteColors::get(noteColorMap, note);
-            noteColor.v = round((velocity * 1.0f / 0x7F) * noteColor.v);
-            break;
-        case RAINBOW: // Generate rainbow-like colors
-            noteColor = CHSV(
-                round((note - noteMin) * (0xFF / (noteMax - noteMin + 1.0f))),
-                0xFF, round((velocity * 1.0f / 0x7F) * 0xFF)
-            );
-            break;
-        case FIXED_COLOR: // Fixed color mapping
-            noteColor = CHSV(fixedHue, 0xFF, round((velocity * 1.0f / 0x7F) * 0xFF));
-            break;
-    };
+// Get the active note color map for a MIDI channel
+MidiNoteColors::Maps MidiColorMapper::getNoteColorMap(uint8_t channel) {
+    return parameters[channel & 0xF].noteColorMap;
+}
+
+// Set the active note color map to use for a MIDI channel
+void MidiColorMapper::setNoteColorMap(uint8_t channel, MidiNoteColors::Maps noteColorMap) {
+    parameters[channel & 0xF].noteColorMap = noteColorMap;
+}
+
+// Get the active fixed color hue for a MIDI channel
+uint8_t MidiColorMapper::getFixedHue(uint8_t channel) {
+    return parameters[channel & 0xF].fixedHue;
+}
+
+// Set the active fixed color hue to use for a MIDI channel
+void MidiColorMapper::setFixedHue(uint8_t channel, uint8_t fixedHue) {
+    parameters[channel & 0xF].fixedHue = fixedHue;
+}
+
+// Get the active velocity ignoring state for a MIDI channel
+bool MidiColorMapper::isIgnoreVelocity(uint8_t channel) {
+    return parameters[channel & 0xF].ignoreVelocity;
+}
+
+// Set the active velocity ignoring state for a MIDI channel
+void MidiColorMapper::setIgnoreVelocity(uint8_t channel, bool state) {
+    parameters[channel & 0xF].ignoreVelocity = state;
+}
+
+// Map a MIDI note message to an HSV color
+struct CHSV MidiColorMapper::map(uint8_t channel, uint8_t note, uint8_t velocity) {
+    struct MidiColorMapperParameters *p = &parameters[channel & 0xF];
+    struct CHSV noteColor = CHSV(0,0,0);
+    if (note >= p->noteMin && note <= p->noteMax) {
+        uint8_t _velocity = p->ignoreVelocity ? 0x7F : velocity & 0x7F;
+        switch (p->mapper) {
+            case COLOR_MAP: // Use Midi Note Color maps
+                noteColor = MidiNoteColors::get(p->noteColorMap, note);
+                noteColor.v = round((_velocity * 1.0f / 0x7F) * noteColor.v);
+                break;
+            case RAINBOW: // Generate rainbow-like colors
+                noteColor = CHSV(
+                    round((note - p->noteMin) * (0xFF / (p->noteMax - p->noteMin + 1.0f))),
+                    0xFF, round((_velocity * 1.0f / 0x7F) * 0xFF)
+                );
+                break;
+            case FIXED_COLOR: // Fixed color mapping
+                noteColor = CHSV(p->fixedHue, 0xFF, round((_velocity * 1.0f / 0x7F) * 0xFF));
+                break;
+        }
+    }
     return noteColor;
+}
+
+// Reset parameters values to defaults for a MIDI channel
+void MidiColorMapper::reset(uint8_t channel) {
+    parameters[channel & 0xF] = DEFAULTS;
 }
--- MidiColorMapper.h
+++ MidiColorMapper.h
@@ -7,27 +7,57 @@
  * MIT license
  */
 
-#include <cinttypes>
 #include <Arduino.h>
+#include <cinttypes>
 #include <MidiNoteColors.h>
 #include <pixeltypes.h>
 
 class MidiColorMapper {
     public:
-        MidiColorMapper();
+        // Available color mappers
         enum Mappers { COLOR_MAP, RAINBOW, FIXED_COLOR };
-        void setNoteMin(uint8_t noteMin);
-        void setNoteMax(uint8_t noteMax);
-        void setMapper(Mappers mapper);
-        void setNoteColorMap(MidiNoteColors::Maps noteColorMap);
-        void setFixedHue(uint8_t hue);
-        struct CHSV map(uint8_t note, uint8_t velocity);
+        
+        // Class constructor
+        MidiColorMapper();
+        
+        // Getter/setters
+        uint8_t getNoteMin(uint8_t channel);
+        void setNoteMin(uint8_t channel, uint8_t noteMin);
+        uint8_t getNoteMax(uint8_t channel);
+        void setNoteMax(uint8_t channel, uint8_t noteMax);
+        Mappers getMapper(uint8_t channel);
+        void setMapper(uint8_t channel, Mappers mapper);
+        MidiNoteColors::Maps getNoteColorMap(uint8_t channel);
+        void setNoteColorMap(uint8_t channel, MidiNoteColors::Maps noteColorMap);
+        uint8_t getFixedHue(uint8_t channel);
+        void setFixedHue(uint8_t channel, uint8_t fixedHue);
+        bool isIgnoreVelocity(uint8_t channel);
+        void setIgnoreVelocity(uint8_t channel, bool state);
+        
+        // Public methods
+        struct CHSV map(uint8_t channel, uint8_t note, uint8_t velocity);
+        void reset(uint8_t channel);
+
     private:
-        uint8_t noteMin;
-        uint8_t noteMax;
-        Mappers mapper;
-        MidiNoteColors::Maps noteColorMap;
-        uint8_t fixedHue;
+        // Parameters per MIDI channel
+        struct MidiColorMapperParameters {
+            uint8_t noteMin;
+            uint8_t noteMax;
+            Mappers mapper;
+            MidiNoteColors::Maps noteColorMap;
+            uint8_t fixedHue;
+            bool ignoreVelocity;
+        } parameters[16];
+        
+        // Default parameters settings
+        const struct MidiColorMapperParameters DEFAULTS = {
+            .noteMin = 0x00,
+            .noteMax = 0x7F,
+            .mapper = COLOR_MAP,
+            .noteColorMap = MidiNoteColors::NEWTON_1704,
+            .fixedHue = 0x00,
+            .ignoreVelocity = true,
+        };
 };
 
 #endif
